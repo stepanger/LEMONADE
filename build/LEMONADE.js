4214fedce1c92f3b5c30ea2a628e8ebc821a26bc
Stepan <creadtive472@gmail.com>
2017-03-15 02:39:38 +1000

    end v 0.0.1 (15 mar)

--- build/LEMONADE.js
+++ build/LEMONADE.js
@@ -11,13 +11,10 @@ START LEMONADE.convert_format()
      * @param {number} Number, число которое стоит отформатировать
      * @param {boolean} Types, тип выводимого числа (number = true, string = false)
      * @param {string} Display, текст после числа 
-     * @return "123 string", 
-               "123 K", 
-                123, 
-                [123, "string"]
+     * @return [12222.222, 12222.2, "Display"]
     */
     
-    myModel.convert_format = function(Number, Types, Display){
+    myModel.convert_format = function(Number, Display){
         
         return LEMONADE.сonverter_of_a_thousand(Number, Display);
     }
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # LIMONADEJS (beta)
 
 ```javascript
-LEMONADE.convert_format("1233", "Mass", "K-likes");
+LEMONADE.convert_format("1233", "K-likes");
 return [1.233, 1.2, "likes"] // kilo (number/1000)
 
-LEMONADE.convert_format("12333333", "Ma", "m-likes");
+LEMONADE.convert_format("12333333", m-likes");
 return [12.333333, 12.3, "likes"] // mega (number/1000000)
 
-LEMONADE.convert_format("1000000001", "Ma", "g-likes");
+LEMONADE.convert_format("1000000001", "g-likes");
 return [1.000000001, 1, "likes"] // giga (number/1000000000)
 
 LEMONADE.transliteration_text("Привет");
