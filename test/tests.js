eb575352c0c3ae2ba250605cf3a931b5f15c22a9
Stepan <creadtive472@gmail.com>
2017-03-15 02:49:00 +1000

    end v 0.0.1 (15 mar)

--- test/tests.js
+++
@@ -1,6 +0,0 @@
-QUnit.test( "validation_of", function( assert ) {
-  assert.ok(LEMONADE.validation_of(123) === 123, "выводит число");
-  assert.ok(LEMONADE.validation_of("123") === 123, "Проверка на строку");
-  assert.ok(LEMONADE.validation_of("asd") === 0, "неверное значение");
-  assert.ok(LEMONADE.validation_of() === 0, "неверное значение");
-});
